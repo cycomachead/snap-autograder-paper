d2542454c0929ae42556a53fa51d9f57cd63f665
Michael Ball <cycomachead@gmail.com>
2015-10-26 15:32:15 -0700

    Fix deployments?

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e # exit with nonzero exit code if anything fails
 
+BRANCH=deploy-pages
 git fetch origin
-git checkout -b gh-pages origin/gh-pages
+git checkout -b ${BRANCH} origin/gh-pages
 
 # inside this git repo we'll pretend to be a new user
 git config --global user.name "Michael Ball (TRAVIS-CI)"
@@ -10,14 +11,16 @@ git config --global user.email "cycomachead@gmail.com"
 
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
-time=`date`
 mv out/* .
 git add .
-git commit -m "Deploy from Travis CI at: ${TIME} (Travis Instance Time)"
+git commit -m "Deploy from Travis CI at: `date` (CI Time)"
 
 # Force push from the current repo's master branch to the remote
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force "https://${GH_TOKEN}@github.com/cycomachead/snap-autograder-paper.git" master:gh-pages > /dev/null 2>&1
+git push --force "https://${GH_TOKEN}@github.com/cycomachead/snap-autograder-paper.git" ${BRANCH}:gh-pages > /dev/null 2>&1
 
+# Cleanup...is this necessary?
+git checkout master
+git branch -D ${BRANCH}
