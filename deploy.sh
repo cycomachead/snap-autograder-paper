845b3dcd22a44c578ba271fbcbfc90c052846058
Michael Ball <cycomachead@gmail.com>
2015-10-26 14:44:03 -0700

    Tweak deploy script at Mon Oct 26 14:44:03 PDT 2015 (testing time)

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -e # exit with nonzero exit code if anything fails
 
-#clear any directories that build scripts create
-rm -rf dist || exit 0
-
 # go to build directory and init a new repo
-cd output
+cd out
 git init
 
 # inside this git repo we'll pretend to be a new user
@@ -14,9 +11,9 @@ git config --global user.email "cycomachead@gmail.com"
 
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
-time=02:27:03 AM
+time=`date`
 git add .
-git commit -m "[ci skip] Deploy via Travis CI at:  (Travis Instance Time)"
+git commit -m "[ci skip] Deploy via Travis CI at: ${TIME} (Travis Instance Time)"
 
 # Force push from the current repo's master branch to the remote
 # repo's gh-pages branch. (All previous history on the gh-pages branch
