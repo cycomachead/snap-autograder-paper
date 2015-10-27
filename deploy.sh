7f9659f8da8882f00d6cfadf68e752175859f41a
Michael Ball <cycomachead@gmail.com>
2015-10-26 18:25:05 -0700

    OMG I cannot type today

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ set -e # exit with nonzero exit code if anything fails
 
 REPO=cycomachead/snap-autograder-paper.git
 DEP_BRANCH=deploy-pages
-GH_BRACH=gh-pages
+GH_BRANCH=gh-pages
 git fetch origin ${GH_BRANCH}
 git checkout -b ${DEP_BRANCH} FETCH_HEAD
 
@@ -21,8 +21,7 @@ git commit -m "Deploy from Travis CI at: `date` (CI Time)"
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force "https://${GH_TOKEN}@github.com/${REPO}" ${DEP_BRANCH}:${GH_BRANCH} 
-# > /dev/null 2>&1
+git push --force "https://${GH_TOKEN}@github.com/${REPO}" ${DEP_BRANCH}:${GH_BRANCH} > /dev/null 2>&1
 
 # Cleanup...is this necessary?
 # git checkout master
